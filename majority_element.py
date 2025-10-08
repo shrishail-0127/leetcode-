54c5241696787f735ebdc21b7b9b3d060f2635ca
shrishail-0127 <shriiipj420@gmail.com>
2025-10-08 21:54:21 +0530

    majority element with boyer moore algorithm

---
+++ majority_element.py
@@ -0,0 +1,32 @@
+'''
+Majority Element
+
+
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+'''
+
+def majority_element(nums):
+    candidate = None
+    count = 0
+    for num in nums:
+        if count == 0:
+            candidate = num
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+    return candidate
